0189ef6e5e0dfac93da25a970c5b22c285460096
cutestsun <118314867+cutestsun@users.noreply.github.com>
2023-04-16 09:24:30 +0000

    Deploying to gh-pages from @ cutestsun/js-practise1@b4c0c9bf3585678191d88564ab6479c5d0ba76b1 🚀

--- index.b1519fc9.js
+++
@@ -1,2 +0,0 @@
-var t={form:document.querySelector("#task-form"),taskList:document.querySelector("#task-list")};function e(t){const e={id:Date.now()};return new FormData(t.currentTarget).forEach(((t,a)=>e[a]=t)),e}var a={addNote:function(t){const e=JSON.parse(localStorage.getItem("notesData"))||[];e.push(t),localStorage.setItem("notesData",JSON.stringify(e))}};t.form.addEventListener("submit",(function(t){t.preventDefault(),a.addNote(e(t))}));
-//# sourceMappingURL=index.b1519fc9.js.map
